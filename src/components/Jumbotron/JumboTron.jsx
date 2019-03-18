9f9fd30fff9d4bb5d1179450db22a839005dfe90
danielifguerrero@gmail.com <danielifguerrero@gmail.com>
2019-03-18 00:01:23 -0500

    jumbotron btn fixed

--- src/components/Jumbotron/JumboTron.jsx
+++ src/components/Jumbotron/JumboTron.jsx
@@ -6,17 +6,18 @@ import './JumboTron.css'
 const JumboTron = () =>{
     return(
         <Jumbotron>
-                    <h1 className='title'> Renta de equipo audiovisual y sonorización profesional</h1>
+                    <h1 className='title'> Renta de equipo de audio e iluminación  </h1>
                     <p className='title_two'>
                       Aseguramos la mejor calidad y potencia para tu evento ! 
                       Cubrimos audiencias de hasta 500 personas.                      
                     </p>
 
                     <p>
-                      <Button style={{float:'right'}} variant="primary" href="mailto:info@interaudio.mx">
+                      <Button className='jumbotron_btn' variant="primary" href="mailto:info@interaudio.mx">
                           Contacto
                       </Button>
                     </p>
+                    <br/>
         </Jumbotron>
     )
 }
