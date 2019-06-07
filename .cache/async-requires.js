f0a1a2d69a192d526036b148619c21864f5315ed
danielifguerrero@gmail.com <danielifguerrero@gmail.com>
2019-06-06 20:12:18 -0500

    center imgs

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/null/Documents/web/interaudio/interaudio_v3/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/null/Documents/web/interaudio/interaudio_v3/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-index-js": () => import("/Users/null/Documents/web/interaudio/interaudio_v3/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-page-2-js": () => import("/Users/null/Documents/web/interaudio/interaudio_v3/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
