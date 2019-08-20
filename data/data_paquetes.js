7f35ec0e1806f04bba87c3746c7e7c56d79fe544
Daniel Flores Guerrero <danielifguerrero@gmail.com>
2019-08-20 15:19:10 -0500

    ya
    ora si

--- data/data_paquetes.js
+++ data/data_paquetes.js
@@ -79,7 +79,7 @@ const main = [
       },
       {
         info: "Sistema de sonido extra para recepción o ceremonias",
-        tip: "2 satelites con 4 bocinas por lado y 1 Subwoofer de 12".
+        tip: "2 satelites con 4 bocinas por lado y 1 Subwoofer de 12"".
       },
       {
         info: "Micrófonos",
