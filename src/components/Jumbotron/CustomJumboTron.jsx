512984d9ddef2e9e5af51ecf7f66ef1108c0acc3
Daniel Flores Guerrero <danielifguerrero@gmail.com>
2019-08-20 18:45:41 -0500

    Update CustomJumboTron.jsx

--- src/components/Jumbotron/CustomJumboTron.jsx
+++ src/components/Jumbotron/CustomJumboTron.jsx
@@ -8,9 +8,8 @@ const CustomJumboTron = () =>{
         <div className='jumbotron_wrapper'>                   
                          <p className='title'> Sonorización profesional para eventos sociales y musicales </p>
 											 
-                       <p className='title_two'>
-															 Audio - Iluminación - Djs - Fiestas - Bodas - XV años - Conciertos - Eventos corporativos - Karaoke - Refuerzo sonoro a bandas y músicos en gral. <br/>
-                                Spoken english.   
+                       <p className='title_two'> Audio - Iluminación - Djs - Fiestas - Bodas - XV años - Conciertos - Eventos corporativos - Karaoke - Refuerzo sonoro a bandas y músicos en gral. <br/>
+                          Spoken english.   
                        </p>
 
                         <p>
