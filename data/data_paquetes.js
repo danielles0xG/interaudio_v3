5eb6f9aa5c31bfe62005dda71d9c8f6d6b4c16c4
Daniel Flores Guerrero <danielifguerrero@gmail.com>
2019-09-01 16:36:16 -0500

    Update data_paquetes.js

--- data/data_paquetes.js
+++ data/data_paquetes.js
@@ -5,39 +5,39 @@ const main = [
     titulo: "OPCION 1: Exclusivo sistema de sonido a 4 vías, Dj/Vdj, Iluminación y Videoproyección HD",
     personas:'(50-170 Personas)',
     subtitulo: 'Potente y exclusivo sistema de sonido a 4 vías, Iluminación robótica, Videoproyección HD y Dj/VDj',
-    precio: "MXN $7,500",
+    precio: "MXN $6,950",
     data: [
       {
         info: "Servicio de Dj con booth iluminado y monitores",
         tip: null
       },
       {
-        info: '4 Bocinas Full-Range de 15"',
-        tip:"Cuatriamplificadas Clase D, diseño coaxial y procesador DSP en red (2000 Watts RMS)"
+        info: '4 Bocinas activas de 15 pulgadas Full-range. 2000 Watts de potencia real (RMS)"',
+        tip:"Cuatri-amplificación Clase D, diseño coaxial de patente y procesador DSP en red"
       },
       {
-        info: '2 Sub graves de 18" activos',
-        tip: "Amplificacion Clase D y procesador DSP (1OOO Wrms)"
+        info: '2 Sub graves de 18 pulgadas activos. 1000 Watts de potencia real (RMS)",
+        tip: "Amplificacion Clase D y procesador DSP en red"
       },
       {
         info: "Mezcladora análoga o digital",
         tip: "De 8, 12 o 16 canales, DSP y gestión inalámbrica"
       },
       {
-        info: "Sistema de sonido extra para recepción o ceremonias",
-        tip: "2 satelites con 4 bocinas por lado y 1 Subwoofer de 12"
+        info: "Sistema de sonido extra para recepción o ceremonias + micrófono",
+        tip: "2 satelites con 4 bocinas por lado y 1 Subwoofer de 15 pulgadas y 1 micro inalámbrico"
       },
       {
         info: "Micrófonos",
-        tip: "Inalámbricos, dinàmicos, de condensador, electret, etc."
+        tip: "Inalámbricos, dinàmicos, de diadema, de condensador, electret, etc."
       },
       {
         info: "Iluminación controlada", 
-        tip: "Pares led, robóticas, barras UV, washes, láser, maquina de humo, etc."
+        tip: "Pares led, mini robóticas, 4 barras led UV, 10 washes 18x3 w, 2 lásers, maquina de humo 1700 W, etc."
       },
       {
-        info: "Videoproyeccion Full HD (Karaoke Pro)",
-        tip: 'Con pantalla de 100 pulgadas ó a pared de hasta 300 ó Pantalla LED UHD Samsung de 65\" '
+        info: "Videoproyeccion Solid Shine Full HD (Karaoke Pro)",
+        tip: 'Con pantalla de 100 pulgadas ó a pared de hasta 300 ó Pantalla SmarTV LED UHD Samsung de 65\" '
       },
       {
         info: "Cableado y accesorios premium",
@@ -46,6 +46,10 @@ const main = [
       {
         info: "Staff técnico e Ing. en audio asignado",
         tip: "Instalación y operación técnica"
+      },
+      {
+        info: "6 horas de servicio",
+        tip: null
       }
     ]
   },
@@ -54,23 +58,19 @@ const main = [
     titulo: "OPCION 2: Potente sistema de sonido a 3 vías, Iluminación y DJ versátil",
     personas:'(30-50 Personas)',
     subtitulo: "Poderoso sistema de sonido a 3 vías con Dj versátil e Iluminación",
-    precio: "MXN $5,500",
+    precio: "MXN $4,950",
     data: [
       {
         info: "Servicio de Dj con booth iluminado y monitores",
         tip: null
       },
       {
-        info: '2 Bocinas activas de 3x8"',
+        info: '2 Bocinas activas, cuatri-amplificadas, coax - 2000 Watts Reales"',
         tip: "Full-range y diseño coaxial, Cuatriamplificadas Clase D (2000 Watts RMS)"
       },
       {
         info: "Mezcladora análoga o digital",
         tip: "De 8, 12 o 16 canales, DSP y gestión inalámbrica"
-      },
-      {
-        info: "Sistema de sonido extra para recepción o ceremonias",
-        tip: "2 satelites con 4 bocinas por lado y 1 Subwoofer de 15\" "
       },
       {
         info: "Micrófonos",
@@ -99,7 +99,7 @@ const main = [
     titulo: "OPCION 3: Pequeño pero poderoso sistema de sonido a 2 vias e Iluminación autorrítmica",
     personas:'(Hasta 30 Personas)',
     subtitulo: "Equipo",
-    precio: "MXN $3,500",
+    precio: "MXN $2,950",
     data: [
       {
         info:'1 Sistema de audio lineal compacto de 450 Watts de potencia y Subwoofer de 15"',
